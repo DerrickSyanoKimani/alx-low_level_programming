da770b1ab7d6f32cd5ea22f932519e94409841b7
DerrickSyanoKimani <derricksyanokimanistudy@proton.me>
2023-02-23 12:44:52 -0800

    Yes!

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,22 +2,12 @@
 
 /**
  * _isupper - checks if parameter is an uppercase character.
- *
- * * Return: 1 if is an uppercase character, 0 in other case.
+ * @c: input character.
+ * Return: 1 if is an uppercase character, 0 in other case.
  */
-
-int _isupper(int c);
-
-/**
- * _isupper - function definition
- * Description: tests case of c
- * @c: character to test
- * Return: 1 if upper 0 if not upper
- */
-
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
+	if (c >= 65 && c <= 90)
 	{
 		return (1);
 	}
